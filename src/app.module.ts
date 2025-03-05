004a1cd89b86be813487a6a0ea6e1f7b9e6be748
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-05 11:19:43 +0200

    add corrections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { UserModule } from './user/user.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
+// decorators contains some meta-data about module
 @Module({
-  imports: [],
+  imports: [UserModule, BookmarkModule],
 })
 export class AppModule {}
