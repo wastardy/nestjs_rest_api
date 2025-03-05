66580a04505852faed230e9ce80ac26bfc65ac8e
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-05 12:24:24 +0200

    init setup main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(process.env.PORT ?? 3333);
+// }
+
+// bootstrap();
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3333);
+  const PORT = process.env.PORT ?? 3333;
+
+  await app.listen(PORT);
+  console.log(`🚀 Server is running on port ${PORT}`);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.log('❌ Error starting server:', err);
+  process.exit(1);
+});
