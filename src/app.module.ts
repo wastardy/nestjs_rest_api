39ae3805d264497b51d2d1ab86d311e69cd4af82
Andrew Kononenko <wastardy.k@gmail.com>
2025-03-06 00:24:53 +0200

    add signin logic, use jwt to generate jwt token

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
 
 // decorators contains some meta-data about module
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ],
 })
 export class AppModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,10 +7,16 @@ import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(dto: AuthDto) {
     // generate the password
@@ -28,7 +34,7 @@ export class AuthService {
       // return saved user
       return {
         message: 'signup service called',
-        user,
+        user: await this.signToken(user.id, user.email),
       };
     } catch (err) {
       if (err instanceof PrismaClientKnownRequestError) {
@@ -43,9 +49,50 @@ export class AuthService {
     }
   }
 
-  signin() {
-    return {
-      message: 'signin service called',
+  async signin(dto: AuthDto) {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email: dto.email,
+        },
+      });
+
+      if (!user)
+        throw new ForbiddenException(
+          'Credentials Incorrect',
+        );
+
+      const passwordsMatches = await argon.verify(
+        user.hash,
+        dto.password,
+      );
+
+      if (!passwordsMatches)
+        throw new ForbiddenException('Password Incorrect');
+
+      return {
+        message: 'signin service called',
+        user: await this.signToken(user.id, user.email),
+      };
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  signToken(
+    userId: number,
+    email: string,
+  ): Promise<string> {
+    const payload = {
+      sub: userId,
+      email,
     };
+
+    const secret = this.config.get('JWT_SECRET');
+
+    return this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
   }
 }
